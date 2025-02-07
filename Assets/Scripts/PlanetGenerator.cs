30029b83c5a3371ece87fd4d71c8cd9a5a2c84ca
James Petersen <jampete@iu.edu>
2025-02-07 17:42:24 -0500

    Moons
    Moon objects can pull the rods out from the planet.

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -13,6 +13,7 @@ public class PlanetGenerator : MonoBehaviour
                                                     //  Resolution max is 128 because ((128 - 1)^2) * 4 = 64516, highest possible resolution without going over max number of vertices
     [SerializeField, Range(0, 1)] float displacementFactor; //Displaces each vertex by 1 / resolution * displacementFactor / 2 in a random vector3 direction
     [SerializeField, Range(0, 1)] float rodLength;
+    [SerializeField, Range(0, 8)] float gravitationalRange = 4;
     [SerializeField] Material material;
 
     public ComputeShader computeShader;
@@ -20,13 +21,20 @@ public class PlanetGenerator : MonoBehaviour
     public ComputeBuffer rodBuffer;
     public ComputeBuffer triangleMeshBuffer;
     public ComputeBuffer vertexMeshBuffer;
+    public ComputeBuffer normalMeshBuffer;
+    public ComputeBuffer moonBuffer;
 
     GameObject[] sectorObjs;
+    GameObject[] moonObjs;
 
     int[] triangles;
     Vector3[] vertices;
+    Vector3[] normals;
     SectorData[] sectors;
     RodData[] rods;
+    MoonData[] moons;
+
+    //Vector3[]
 
     struct SectorData { //  Rod Count (resolution-1)^2
         public int id;
@@ -45,18 +53,42 @@ public class PlanetGenerator : MonoBehaviour
         public float surfaceArea;
         public Vector3 center;
         public Vector3 perpendicularity;
+        public float displacement;
+    }
+    struct MoonData {
+        public Vector3 position;
+        public float size;
     }
 
-    float phi;
-    Vector3[] icosahedronVertices;
     private void Awake() {
         this.sectors = InitializeSectors();
-        Compute();
+        InitializeMoons();
+        ComputePlanet();
         Render();
     }
+
+    private void Update() {
+        if (CheckMoonDataChanged()) {
+            PullRods();
+        }
+    }
+    private bool CheckMoonDataChanged() {
+        bool changed = false;
+        for (int i = 0; i < moonObjs.Length; i++) {
+            if (moonObjs[i].transform.position != moons[i].position) {
+                changed = true;
+                moons[i].position = moonObjs[i].transform.position;
+            }
+            if (moonObjs[i].transform.localScale.x != moons[i].size) {
+                changed = true;
+                moons[i].size = moonObjs[i].transform.localScale.x;
+            }
+        }
+        return changed;
+    }
     private SectorData[] InitializeSectors() {
-        phi = (1 + Mathf.Sqrt(5)) / 2;
-        icosahedronVertices = new Vector3[12] {
+        float phi = (1 + Mathf.Sqrt(5)) / 2;
+        Vector3[] icosahedronVertices = new Vector3[12] {
         new Vector3(-1,  phi, 0),
         new Vector3( 1,  phi, 0),
         new Vector3(-1, -phi, 0),
@@ -98,17 +130,31 @@ public class PlanetGenerator : MonoBehaviour
 
         return sector;
     }
+    private void InitializeMoons() {
+        moonObjs = GameObject.FindGameObjectsWithTag("Moon");
+        MoonData[] moonDatas = new MoonData[moonObjs.Length];
+        for (int i = 0; i < moonDatas.Length; i++) {
+            moonDatas[i] = new MoonData();
+            moonDatas[i].position = moonObjs[i].transform.position;
+            moonDatas[i].size = moonObjs[i].transform.localScale.x;
+        }
+        moons = moonDatas;
+        computeShader.SetInt("moonCount", moons.Length);
+        //Debug.Log(moons[0].size + " " + moons[0].position);
+        moonBuffer = new ComputeBuffer(moons.Length, sizeof(float) * 4);
+    }
 
-    private void Compute() {
+    private void ComputePlanet() {
         int kernelHandle0 = computeShader.FindKernel("SubdivideSector");
         int kernelHandle1 = computeShader.FindKernel("PopScatterSector");
 
         int rodCountPerSector = (int)Mathf.Pow(resolution - 1, 2);
         int rodCountTotal = rodCountPerSector * 20;
         sectorBuffer = new ComputeBuffer(20, sizeof(int) + sizeof(float) * 3 * 3);
-        rodBuffer = new ComputeBuffer(rodCountTotal, sizeof(int) + sizeof(float) * 9 + sizeof(float) * 9 + sizeof(float) + sizeof(float) * 3 + sizeof(float) * 3);
+        rodBuffer = new ComputeBuffer(rodCountTotal, sizeof(int) + sizeof(float) * 9 + sizeof(float) * 9 + sizeof(float) + sizeof(float) * 3 + sizeof(float) * 3 + sizeof(float));
         triangleMeshBuffer = new ComputeBuffer(rodCountPerSector * 21, sizeof(int));
         vertexMeshBuffer = new ComputeBuffer(rodCountTotal * 6, sizeof(float) * 3);
+        normalMeshBuffer = new ComputeBuffer(rodCountTotal * 6, sizeof(float) * 3);
 
         computeShader.SetBuffer(kernelHandle0, "sectors", sectorBuffer);
         sectorBuffer.SetData(sectors);
@@ -121,12 +167,12 @@ public class PlanetGenerator : MonoBehaviour
         computeShader.SetFloat("displacementFactor", displacementFactor);
         computeShader.SetFloat("rodLength", rodLength);
 
-        int threadGroupSize = Mathf.CeilToInt(rodCountTotal * 20 / 64.0f);
+        int threadGroupSize = Mathf.CeilToInt(rodCountTotal / 64.0f);
         computeShader.Dispatch(kernelHandle0, threadGroupSize, 1, 1);
 
-        computeShader.SetBuffer(kernelHandle1, "sectors", sectorBuffer);
         computeShader.SetBuffer(kernelHandle1, "rods", rodBuffer);
         computeShader.SetBuffer(kernelHandle1, "vertices", vertexMeshBuffer);
+        computeShader.SetBuffer(kernelHandle1, "normals", normalMeshBuffer);
         computeShader.Dispatch(kernelHandle1, threadGroupSize, 1, 1);
 
         SectorData[] updatedSectors = new SectorData[20];
@@ -144,15 +190,13 @@ public class PlanetGenerator : MonoBehaviour
         Vector3[] updatedVertices = new Vector3[rodCountTotal * 6];
         vertexMeshBuffer.GetData(updatedVertices);
         vertices = updatedVertices;
+
+        Vector3[] updatedNormals = new Vector3[rodCountTotal * 6];
+        normalMeshBuffer.GetData(updatedNormals);
+        normals = updatedNormals;
     }
 
     private void Render() {
-
-        // Returned Buffers from the compute will have a single buffer with the vertices and a single buffer with the triangles
-        // Need to separate those out into 20 different arrays
-        //Alternatively...
-        //  Triangulation and Vertex Data will be the exact same for all of the different meshes.
-        //  Reuse
 
         sectorObjs = new GameObject[sectors.Length];
         int sectorLength = vertices.Length / sectors.Length; // Divide vertices equally among sectors
@@ -171,8 +215,12 @@ public class PlanetGenerator : MonoBehaviour
             mesh.vertices = sectorVertices;
 
             // Ensure triangles and vertex count match
-            mesh.triangles = triangles; // Make sure this is correctly set for each sector
-            mesh.RecalculateNormals();
+            mesh.triangles = triangles;
+
+            Vector3[] sectorNormals = new Vector3[sectorLength]; // This should match the expected number of vertices per sector
+            Array.Copy(vertices, i * sectorLength, sectorNormals, 0, sectorLength); // Copy the correct chunk of vertices for this sector
+            mesh.normals = sectorNormals;
+
             mesh.RecalculateTangents();
 
             // Add components to the sector object
@@ -181,17 +229,56 @@ public class PlanetGenerator : MonoBehaviour
         }
     }
 
+    private void PullRods() {
+        if (moons.Length > 0) {
+            //Debug.Log("Pulling Rods");
+            int kernelHandle = computeShader.FindKernel("MoonAffect");
+
+            moonBuffer.SetData(moons);
+            computeShader.SetFloat("gravitationalRange", gravitationalRange);
+
+            computeShader.SetBuffer(kernelHandle, "rods", rodBuffer);
+            computeShader.SetBuffer(kernelHandle, "vertices", vertexMeshBuffer);
+            computeShader.SetBuffer(kernelHandle, "moons", moonBuffer);
+
+            int threadGroupSize = Mathf.CeilToInt((float)rods.Length / 64.0f);
+            computeShader.Dispatch(kernelHandle, threadGroupSize, 1, 1);
+
+            Vector3[] updatedVertices = new Vector3[vertices.Length];
+            vertexMeshBuffer.GetData(updatedVertices);
+            vertices = updatedVertices;
+
+            int sectorLength = vertices.Length / sectors.Length;
+
+            // Assign updated vertex data to each sector
+            for (int i = 0; i < sectors.Length; i++) {
+                Mesh mesh = sectorObjs[i].GetComponent<MeshFilter>().mesh;
+
+                Vector3[] sectorVertices = new Vector3[sectorLength];
+                Array.Copy(vertices, i * sectorLength, sectorVertices, 0, sectorLength);
+
+                mesh.vertices = sectorVertices;
+                mesh.RecalculateBounds();
+            }
+        }
+    }
+
+
     private void OnDestroy() {
         if (sectorBuffer != null) sectorBuffer.Release();
         if (rodBuffer != null) rodBuffer.Release();
         if (triangleMeshBuffer != null) triangleMeshBuffer.Release();
         if (vertexMeshBuffer != null) vertexMeshBuffer.Release();
+        if (normalMeshBuffer != null) normalMeshBuffer.Release();
+        if (moonBuffer != null) moonBuffer.Release();
     }
     private void OnDisable() {
         if (sectorBuffer != null) sectorBuffer.Release();
         if (rodBuffer != null) rodBuffer.Release();
         if (triangleMeshBuffer != null) triangleMeshBuffer.Release();
         if (vertexMeshBuffer != null) vertexMeshBuffer.Release();
+        if (normalMeshBuffer != null) normalMeshBuffer.Release();
+        if (moonBuffer != null) moonBuffer.Release();
     }
 
 
